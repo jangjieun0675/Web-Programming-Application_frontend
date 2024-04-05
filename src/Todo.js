0bb74a42002e1ea99b384f836659d42d758dea61
jangjieun0675 <jangji0513@naver.com>
2024-04-06 01:59:14 +0900

    Todo.js create

---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Todo = () => {
+  return(
+    <>
+    <div className="Todo">
+      <input type="checkbox" id="todo" name="todo0" value="todo0"/>
+      <label for="todo0">Todo 컴포넌트 만들기</label>
+    </div>
+    <div><h1>장지은</h1></div>
+    </>
+  );
+};
+
+export default Todo;
